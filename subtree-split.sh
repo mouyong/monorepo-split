a30fe192e0eb0f853dad27480ee268315b721584
mouyong <mouyong@users.noreply.github.com>
2022-10-18 16:50:04 +0800

    Update subtree-split.sh

--- subtree-split.sh
+++ subtree-split.sh
@@ -9,8 +9,8 @@ function split()
 {
     git subtree split -P $1 -b $2
     git push $2 "$2:refs/heads/$CURRENT_BRANCH" -f
-    git remote remove $2
-    git branch -D $2
+    git remote remove $2 || true
+    git branch -D $2 || true
 }
 
 function remote()
