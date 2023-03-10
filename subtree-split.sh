bc8ac6fd7dc32b39992c4d50ebd9755811bb4466
mouyong <mouyong@users.noreply.github.com>
2023-03-10 09:16:26 +0800

    Update subtree-split.sh

--- subtree-split.sh
+++ subtree-split.sh
@@ -24,5 +24,6 @@ git pull origin $CURRENT_BRANCH || true
 # remote foundation git@github.com:mouyong/laravel-foundation.git
 remote MarketManager git@github.com:mouyong/MarketManager.git
 
+
 # split 'src/Illuminate/Foundation' foundation
 split 'MarketManager' MarketManager
